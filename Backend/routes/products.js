3d8f12feeae7a644046dacdff6d8af834f712a1d
DavitaR <92464615+DavitaR@users.noreply.github.com>
2022-06-15 11:07:37 -0400

    middlewares on products routes

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -5,14 +5,14 @@ const validator = require("../DTO/validator");
 const productSchema = require("../DTO/productInfoValidation/productSchema");
 const checkProduct = require("../middlewares/checkProduct");
 const adminCheck = require("../middlewares/adminCheck");
+const authCheck = require("../middlewares/authCheck")
 const SearchController = require("../controllers/search");
 
 route.get("/", ProductController.getAllProducts);
 route.get("/search/limit", SearchController.searchLimit);
 route.get("/:productId", checkProduct, ProductController.getProductById);
-route.put("/:productId", checkProduct, ProductController.updateProduct);
-route.use(adminCheck);
-route.post("/", validator(productSchema), ProductController.addNewProduct);
-route.delete("/:productId", checkProduct, ProductController.deleteProduct);
+route.put("/:productId", authCheck, adminCheck, checkProduct, ProductController.updateProduct);
+route.post("/", authCheck, adminCheck, validator(productSchema), ProductController.addNewProduct);
+route.delete("/:productId", authCheck, adminCheck, checkProduct, ProductController.deleteProduct);
 
 module.exports = route;
