324cfd4bba001d23982abf0320a6af5d1e313376
Yael Tzruia <yaeli.rogestav@gmail.com>
2022-06-15 15:00:48 +0300

    search limit five

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -26,7 +26,7 @@ const register = async (req, res, next) => {
   }
 };
 
-const login = async (req, res, next) => {
+const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -4,10 +4,15 @@ const ProductController = require("../controllers/products");
 const validator = require("../DTO/validator");
 const productSchema = require("../DTO/productInfoValidation/productSchema");
 const checkProduct = require("../middlewares/checkProduct");
+const adminCheck = require("../middlewares/adminCheck");
+const SearchController = require("../controllers/search");
 
 route.get("/", ProductController.getAllProducts);
+route.get("/search/limit", SearchController.searchLimit);
 route.get("/:productId", checkProduct, ProductController.getProductById);
 route.put("/:productId", checkProduct, ProductController.updateProduct);
+route.use(adminCheck);
 route.post("/", validator(productSchema), ProductController.addNewProduct);
 route.delete("/:productId", checkProduct, ProductController.deleteProduct);
+
 module.exports = route;
